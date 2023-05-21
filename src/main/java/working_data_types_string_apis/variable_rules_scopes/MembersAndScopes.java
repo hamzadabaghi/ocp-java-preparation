78db6a66078f12ede50f0d2ed3e84808f39e1eb5
hamzadabaghi <hamza.dabaghi@gmail.com>
2023-05-21 14:22:49 +0100

    Members and Member scope.

---
+++ src/main/java/working_data_types_string_apis/variable_rules_scopes/MembersAndScopes.java
@@ -0,0 +1,15 @@
+package working_data_types_string_apis.variable_rules_scopes;
+
+public class MembersAndScopes {
+
+    public char f = 'A'; // the same module
+    protected boolean l = true; // same package or in subtypes
+    long j = 12; // default: package-private
+    /* ------ members = fields + methods except block initializers and constructors ---------- */
+    private int i = 10;  // private : class only
+
+    public static void main(String[] args) {
+
+
+    }
+}
