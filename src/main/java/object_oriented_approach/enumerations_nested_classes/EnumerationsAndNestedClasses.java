c41072d12c9e99250ede6846ab6ff37a5a1fd90a
hamzadabaghi <hamza.dabaghi@gmail.com>
2023-05-29 23:15:54 +0100

    add an enumeration.

---
+++ src/main/java/object_oriented_approach/enumerations_nested_classes/EnumerationsAndNestedClasses.java
@@ -0,0 +1,29 @@
+package object_oriented_approach.enumerations_nested_classes;
+
+public class EnumerationsAndNestedClasses {
+
+
+    public static void main(String[] args) {
+
+        Role role = Role.ADMIN;
+        System.out.println(role.name() + "--" + role.getPriority());
+
+    }
+
+    // define an enumeration
+    enum Role {
+        ADMIN(1), USER(2), GUEST(3);
+
+        private final int priority;
+
+        Role(int priority) {
+            this.priority = priority;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+    }
+}
+    }
+
