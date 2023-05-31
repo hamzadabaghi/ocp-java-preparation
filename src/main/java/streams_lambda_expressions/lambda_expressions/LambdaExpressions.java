f04b26406346212b2c0302ed26f652ee553c1a36
hamzadabaghi <hamza.dabaghi@gmail.com>
2023-05-31 06:28:30 +0100

    use functional programming

--- src/main/java/streams_lambda_expressions/lambda_expressions/LambdaExpressions.java
+++ src/main/java/streams_lambda_expressions/lambda_expressions/LambdaExpressions.java
@@ -1,7 +1,10 @@
 package streams_lambda_expressions.lambda_expressions;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 public class LambdaExpressions {
@@ -9,16 +12,23 @@ public class LambdaExpressions {
 
     public static void main(String[] args) {
 
+        Map<Category, List<Product>> productsByCategory = new HashMap<>();
         /* ------- capture variables in a lambda expression ------- */
         List<Product> products = ExampleData.getProducts();
         BigDecimal priceLimit = new BigDecimal("5.00");
         // priceLimit = null; // this won't work
         Predicate<Product> filterProducts = product -> product.getPrice().compareTo(priceLimit) < 0;
         // priceLimit = null; // this won't work even after declaration of the lambda expression
-        products.stream()
-                .filter(filterProducts)
-                .forEach(System.out::println);
+//        products.stream()
+//                .filter(filterProducts)
+//                .forEach(System.out::println);
 
+
+        /* ----------- using other functional interfaces ---------- */
+
+        products.forEach(product -> productsByCategory.computeIfAbsent(product.getCategory(), c -> new ArrayList<>()).add(product));
+
+        System.out.println(products);
 
     }
 
